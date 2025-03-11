1ec2db7ba258e085626725c95b904e4d2b1c8f8b
nimi1125 <sikaku1111@gmail.com>
2025-03-11 18:39:57 +0900

    update package-lock.json package.json vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-const { defineConfig } = require('vite');
-const vue = require('@vitejs/plugin-vue');
-const laravel = require('laravel-vite-plugin');
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import laravel from 'laravel-vite-plugin';
 
-module.exports = defineConfig({
+export default defineConfig({
     plugins: [
         vue(), 
         laravel({
